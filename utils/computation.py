b9f47743b95a66f373dcc913f158f032d435b6ce
baptistevie <baptiste.vie.bvbv@gmail.com>
2025-07-08 11:19:01 -0400

    modif intermediaire de computation

--- utils/computation.py
+++ utils/computation.py
@@ -3,151 +3,212 @@ from scipy.spatial import Delaunay
 from scipy.spatial import Voronoi
 from collections import defaultdict
 
-def afficher(id_to_track, candidates,message):
-     if not all(x in candidates for x in id_to_track): #inclusion
-        lost_ids = [id for id in id_to_track if id not in candidates]
-        print("Perte de candidats : ",message," : ", lost_ids)
-        return [id for id in id_to_track if id in candidates]
-     return id_to_track
-     
-def computation(positions,ages, width, height):
-    """On prend en entrée un dictionnaire avec les positions des objets détectés"""
-    # On extrait les points des positions
-    points = np.array([pos for pos in positions.values() if pos is not None])
-    ages = np.array([ages[obj] for obj in positions.keys() if positions[obj] is not None])
-    if len(points) < 3:
-        print(f"\rPas assez de points uniques pour la triangulation (avant filtrage).", end="")
-        return None, None, None, None
-    # On exclut les points qui sont en dehors de la scène
-    mask=((points[:, 0] >= 0) & (points[:, 0] <= width) & 
-                    (points[:, 1] >= 0) & (points[:, 1] <= height)&(ages>30))
-    points = points[mask]
-    # On s'assure que les points sont uniques
-    points = np.unique(points, axis=0)
+class Computation():
+    def __init__(self):
+        self.points = None
+        self.tri = None
+        self.triangle_counts = None
+        self.areas = None
+        self.candidates = None
+        self.empty_triangles = None
 
-    # Si moins de 3 points uniques, on ne peut pas faire de triangulation
-    if len(points) < 3:
-        print(f"\rPas assez de points uniques pour la triangulation (après filtrage).", end="")
-        return None, None, None, None
-    # Exclusion de point spécifiques, pour des tests ##################################################
-    points_a_exclure = [10]
-    points = np.array([point for i, point in enumerate(points) if i not in points_a_exclure])
-    # Triangulation de Delaunay
-    tri = Delaunay(points)
 
-    
-    triangle_counts = defaultdict(int)
+    def afficher(id_to_track, candidates,message):
+        if not all(x in candidates for x in id_to_track): #inclusion
+            lost_ids = [id for id in id_to_track if id not in candidates]
+            print("Perte de candidats : ",message," : ", lost_ids)
+            return [id for id in id_to_track if id in candidates]
+        return id_to_track
+        
+    def computation(positions,ages, width, height):
+        """On prend en entrée un dictionnaire avec les positions des objets détectés"""
+        # On extrait les points des positions
+        points = np.array([pos for pos in positions.values() if pos is not None])
+        ages = np.array([ages[obj] for obj in positions.keys() if positions[obj] is not None])
+        if len(points) < 3:
+            print(f"\rPas assez de points uniques pour la triangulation (avant filtrage).", end="")
+            return None, None, None, None
+        # On exclut les points qui sont en dehors de la scène
+        mask=((points[:, 0] >= 0) & (points[:, 0] <= width) & 
+                        (points[:, 1] >= 0) & (points[:, 1] <= height)&(ages>30))
+        points = points[mask]
+        # On s'assure que les points sont uniques
+        points = np.unique(points, axis=0)
 
-    for simplex in tri.simplices:
-        for vertex in simplex:
-            triangle_counts[vertex] += 1
+        # Si moins de 3 points uniques, on ne peut pas faire de triangulation
+        if len(points) < 3:
+            print(f"\rPas assez de points uniques pour la triangulation (après filtrage).", end="")
+            return None, None, None, None
+        # Exclusion de point spécifiques, pour des tests ##################################################
+        points_a_exclure = [8]
+        points = np.array([point for i, point in enumerate(points) if i not in points_a_exclure])
+        # Triangulation de Delaunay
+        tri = Delaunay(points)
 
-    # Voronoï diagram
-    vor = Voronoi(points)
+        
+        triangle_counts = defaultdict(int)
 
-    areas = {}
-    for idx, region_index in enumerate(vor.point_region):
-        region = vor.regions[region_index]
-        if not -1 in region and len(region) > 0:
-            polygon = [vor.vertices[i] for i in region]
+        for simplex in tri.simplices:
+            for vertex in simplex:
+                triangle_counts[vertex] += 1
+
+        # Voronoï diagram
+        vor = Voronoi(points)
+
+        areas = {}
+        for idx, region_index in enumerate(vor.point_region):
+            region = vor.regions[region_index]
+            if not -1 in region and len(region) > 0:
+                polygon = [vor.vertices[i] for i in region]
+                area = 0.5 * np.abs(
+                    np.dot([p[0] for p in polygon], np.roll([p[1] for p in polygon], 1)) -
+                    np.dot([p[1] for p in polygon], np.roll([p[0] for p in polygon], 1))
+                )
+                areas[idx] = area
+            else:
+                areas[idx] = np.inf  # Cellule infinie → point au bord
+
+        return points,tri,triangle_counts,areas
+
+    def personnes_centrales(points,tri, triangle_counts,areas,n_triangles,distance_min,angle_max,id_to_track = []):
+        """Cherche les candidats qui puissent être au centre des zones"""
+        
+        if tri is None or triangle_counts is None:
+            return None
+        # On ne conserve que les candidats à l'aire non infinie
+        candidates = {idx: count for idx, count in triangle_counts.items() if areas.get(idx, np.inf) != np.inf}
+        id_to_track = afficher(id_to_track, candidates,"aire infinie")
+        if not candidates:
+            return None
+        # On garde uniquement les candidats avec plus de n_triangles triangles
+        candidates = {idx: count for idx, count in candidates.items() if count >= n_triangles}
+        id_to_track = afficher(id_to_track, candidates,f"moins de {n_triangles} triangles")
+        if not candidates:
+            return None
+        # Parmi les candidats restants, on enlève ceux qui sont proches d'un autre point (dans points) qui n'est pas candidat
+        candidate_indices = list(candidates.keys())
+        filtered_candidates = []
+        for idx in candidate_indices:
+            point = points[idx]
+            is_far = True
+            for other_idx, other_point in enumerate(points):
+                if other_idx not in candidate_indices:
+                    distance = np.linalg.norm(point - other_point)
+                    if distance < distance_min:
+                        is_far = False
+                        break
+            if is_far:
+                filtered_candidates.append(idx)
+        # rendre unique filtred_candidates
+        filtered_candidates = list(set(filtered_candidates))
+        id_to_track = afficher(id_to_track, filtered_candidates,f"proche d'un autre point (distance < {distance_min})")
+        if len(filtered_candidates) == 0:
+            return None
+        # Si on a un seul candidat, on le retourne
+        if len(filtered_candidates) == 1:
+            return filtered_candidates
+
+        # S'il reste plusieurs candidats, on vérifie qu'il n'y a pas de faux positifs parmis eux. 
+        # D'abord on liste tous les points non candidats, voisins de candidats
+        non_candidate_neighbors = []
+        for idx in filtered_candidates:
+            point = points[idx]
+            for other_idx, other_point in enumerate(points):
+                if other_idx not in filtered_candidates:
+                    if other_idx not in non_candidate_neighbors:
+                        non_candidate_neighbors.append(other_idx)
+        # On calcule les angles des vecteurs candidats - voisins non candidats
+        #print(f"Nombre de candidats après filtrage : {len(filtered_candidates)}")
+        final_candidates = []
+
+        #print(f"Nombre de candidats après filtrage : {len(filtered_candidates)}")
+        #print(f"Nombre de voisins non candidats : {len(non_candidate_neighbors)}")
+        for idx in filtered_candidates:
+            #print(f"Vérification des angles pour le candidat {idx}")
+            angles = []
+            point = points[idx]
+            for other_idx in non_candidate_neighbors:
+                other_point = points[other_idx]
+                vector = other_point - point
+                angle = np.arctan2(vector[1], vector[0]) * 180 / np.pi+180
+                angles.append(angle)
+            angles = np.sort(np.array(angles))
+            #print(f"Angles pour le candidat {idx}: {angles}")
+            differences = np.diff(angles)
+            # On vérifie si l'angle minimum est respecté
+            #print(f"Angle max pour le candidat {idx}: {max(differences)}")
+            if max(differences) < angle_max and 360 + angles[0] - angles[-1] < angle_max: #cas où le dernier angle est proche du premier
+                final_candidates.append(idx)
+        id_to_track = afficher(id_to_track, final_candidates,f"angle max {angle_max}°")
+        #print(f"Nombre de candidats après vérification des angles : {len(filtered_candidates)}")
+
+
+        return final_candidates if final_candidates else None
+
+    def empty_zones(points,tri,area_threshold=4):
+        # On récupère la triangulation de Delaunay, et on calcule les aires des triangles : {idx: [point1, point2, point3]}
+        if tri is None or points is None:
+            return None
+        empty_triangles = {}
+        idx = 0
+        for simplex in tri.simplices:
+            pts = points[simplex]
             area = 0.5 * np.abs(
-                np.dot([p[0] for p in polygon], np.roll([p[1] for p in polygon], 1)) -
-                np.dot([p[1] for p in polygon], np.roll([p[0] for p in polygon], 1))
+                np.dot([p[0] for p in pts], np.roll([p[1] for p in pts], 1)) -
+                np.dot([p[1] for p in pts], np.roll([p[0] for p in pts], 1))
             )
-            areas[idx] = area
-        else:
-            areas[idx] = np.inf  # Cellule infinie → point au bord
+            if area > area_threshold:
+                empty_triangles[idx] = simplex.tolist()
+                idx += 1
+        return empty_triangles
 
-    return points,tri,triangle_counts,areas
+    def edge_length(p1, p2):
+        return np.linalg.norm(p1 - p2)
 
-def personnes_centrales(points,tri, triangle_counts,areas,n_triangles,distance_min,angle_max,id_to_track = []):
-    """Cherche les candidats qui puissent être au centre des zones"""
-    
-    if tri is None or triangle_counts is None:
-        return None
-    # On ne conserve que les candidats à l'aire non infinie
-    candidates = {idx: count for idx, count in triangle_counts.items() if areas.get(idx, np.inf) != np.inf}
-    id_to_track = afficher(id_to_track, candidates,"aire infinie")
-    if not candidates:
-        return None
-    # On garde uniquement les candidats avec plus de n_triangles triangles
-    candidates = {idx: count for idx, count in candidates.items() if count >= n_triangles}
-    id_to_track = afficher(id_to_track, candidates,f"moins de {n_triangles} triangles")
-    if not candidates:
-        return None
-    # Parmi les candidats restants, on enlève ceux qui sont proches d'un autre point (dans points) qui n'est pas candidat
-    candidate_indices = list(candidates.keys())
-    filtered_candidates = []
-    for idx in candidate_indices:
-        point = points[idx]
-        is_far = True
-        for other_idx, other_point in enumerate(points):
-            if other_idx not in candidate_indices:
-                distance = np.linalg.norm(point - other_point)
-                if distance < distance_min:
-                    is_far = False
-                    break
-        if is_far:
-            filtered_candidates.append(idx)
-    # rendre unique filtred_candidates
-    filtered_candidates = list(set(filtered_candidates))
-    id_to_track = afficher(id_to_track, filtered_candidates,f"proche d'un autre point (distance < {distance_min})")
-    if len(filtered_candidates) == 0:
-        return None
-    # Si on a un seul candidat, on le retourne
-    if len(filtered_candidates) == 1:
-        return filtered_candidates
+    def get_edge_indices(triangle):
+        # Retourne les paires d’indices d’arêtes d’un triangle (3 côtés)
+        return [(triangle[0], triangle[1]),
+                (triangle[1], triangle[2]),
+                (triangle[2], triangle[0])]
+    def expansion(triangles,tri, points, ratio_threshold=1.5):
+        '''On a détecté de premiers triangles, on va également étendre aux triangles voisins, si ceux si répondent au critère : 
+        la somme des deux nouveaux côtés est inférieure ratio_threshold fois le coté en commun'''
+        if tri is None or points is None or triangles is None:
+            return None
+        region = set(triangles.index())
+        visited = set()
+        to_visit = list(region)
 
-    # S'il reste plusieurs candidats, on vérifie qu'il n'y a pas de faux positifs parmis eux. 
-    # D'abord on liste tous les points non candidats, voisins de candidats
-    non_candidate_neighbors = []
-    for idx in filtered_candidates:
-        point = points[idx]
-        for other_idx, other_point in enumerate(points):
-            if other_idx not in filtered_candidates:
-                if other_idx not in non_candidate_neighbors:
-                    non_candidate_neighbors.append(other_idx)
-    # On calcule les angles des vecteurs candidats - voisins non candidats
-    #print(f"Nombre de candidats après filtrage : {len(filtered_candidates)}")
-    final_candidates = []
+        while to_visit:
+            current_idx = to_visit.pop()
+            visited.add(current_idx)
+            current_triangle = tri.simplices[current_idx]
 
-    #print(f"Nombre de candidats après filtrage : {len(filtered_candidates)}")
-    #print(f"Nombre de voisins non candidats : {len(non_candidate_neighbors)}")
-    for idx in filtered_candidates:
-        #print(f"Vérification des angles pour le candidat {idx}")
-        angles = []
-        point = points[idx]
-        for other_idx in non_candidate_neighbors:
-            other_point = points[other_idx]
-            vector = other_point - point
-            angle = np.arctan2(vector[1], vector[0]) * 180 / np.pi+180
-            angles.append(angle)
-        angles = np.sort(np.array(angles))
-        #print(f"Angles pour le candidat {idx}: {angles}")
-        differences = np.diff(angles)
-        # On vérifie si l'angle minimum est respecté
-        #print(f"Angle max pour le candidat {idx}: {max(differences)}")
-        if max(differences) < angle_max and 360 + angles[0] - angles[-1] < angle_max: #cas où le dernier angle est proche du premier
-            final_candidates.append(idx)
-    id_to_track = afficher(id_to_track, final_candidates,f"angle max {angle_max}°")
-    #print(f"Nombre de candidats après vérification des angles : {len(filtered_candidates)}")
+            for i, neighbor_idx in enumerate(tri.neighbors[current_idx]):
+                if neighbor_idx == -1 or neighbor_idx in visited or neighbor_idx in region:
+                    continue
 
+                neighbor_triangle = tri.simplices[neighbor_idx]
 
-    return final_candidates if final_candidates else None
+                # Trouver l’arête commune
+                shared = set(current_triangle) & set(neighbor_triangle)
+                if len(shared) != 2:
+                    continue  # doivent partager exactement une arête
 
-def empty_zones(points,tri,area_threshold=4):
-    # On récupère la triangulation de Delaunay, et on calcule les aires des triangles : {idx: [area, [point1, point2, point3], [i1, i2, i3]]}
-    if tri is None or points is None:
-        return None
-    empty_triangles = {}
-    idx = 0
-    for simplex in tri.simplices:
-        pts = points[simplex]
-        area = 0.5 * np.abs(
-            np.dot([p[0] for p in pts], np.roll([p[1] for p in pts], 1)) -
-            np.dot([p[1] for p in pts], np.roll([p[0] for p in pts], 1))
-        )
-        if area > area_threshold:
-            empty_triangles[idx] = [area]+ simplex.tolist()
-            idx += 1
-    return empty_triangles
+                a, b = list(shared)
+                edge = edge_length(points[a], points[b])
+
+                # Points restants (deux nouveaux côtés du triangle voisin)
+                other_points = list(set(neighbor_triangle) - shared)
+                if len(other_points) != 1:
+                    continue
+                c = other_points[0]
+
+                d1 = edge_length(points[a], points[c])
+                d2 = edge_length(points[b], points[c])
+
+                if d1 + d2 < ratio_threshold * edge:
+                    region.add(neighbor_idx)
+                    to_visit.append(neighbor_idx)
+
+        return list(region)
